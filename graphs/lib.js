be9119b5a1b937ce9f3eb82d4db9eabdd156359d
Niklas Frykholm <niklas.frykholm@autodesk.com>
2016-01-25 20:27:59 +0100

    Pipeline illustrations

--- graphs/lib.js
+++ graphs/lib.js
@@ -1,46 +1,69 @@
+class Point
+{
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Rect
+{
+    constructor(x,y,w,h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+
+    get left() {return this.x;}
+    get center() {return this.x + this.w/2;}
+    get right() {return this.x + this.w;}
+
+    get top() {return this.y;}
+    get middle() {return this.y + this.h/2;}
+    get bottom() {return this.y + this.h;}
+
+    get bottomCenter() {return new Point(this.center, this.bottom);}
+    get topCenter() {return new Point(this.center, this.top);}
+
+    offset(x,y) {return new Rect(this.x+x, this.y+y, this.w, this.h);}
+}
+
 var draw = {
-  textbox: function(ctx, text, x, y, w, h) {
-    x = x || 0;
-    y = y || 0;
-    w = w || 200;
-    h = h || 50;
+  textbox: function(ctx, text, r) {
     ctx.fillStyle = "#eee";
-    ctx.fillRect(x,y,w,h);
+    ctx.fillRect(r.x,r.y,r.w,r.h);
     ctx.fillStyle = "#000";
-    ctx.strokeRect(x,y,w,h);
+    ctx.strokeRect(r.x,r.y,r.w,r.h);
     ctx.font = "18px Arial";
-    var tm = ctx.measureText(text);
-
-    ctx.fillText(text, x+w/2-tm.width/2, y+h/2+18/2);
-
-    return {
-      x: x, y: y, w: w, h: h,
-      left: x, right: x+w, center: x+w/2,
-      top: y, bottom: y+h, middle: y+h/2,
-    };
+    ctx.textBaseline = "middle";
+    ctx.textAlign = "center";
+    ctx.fillText(text, r.center, r.middle);
   },
-  arrow: function(ctx, x1, y1, x2, y2, doubleArrow) {
+  arrow: function(ctx, p1, p2, doubleArrow) {
     var w = 10;
     var h = 20;
 
     ctx.beginPath();
-    ctx.moveTo(x1, y1);
-    ctx.lineTo(x2, y2);
+    ctx.moveTo(p1.x, p1.y);
+    ctx.lineTo(p2.x, p2.y);
 
-    var len = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-    var dx = (x2-x1)/len;
-    var dy = (y2-y1)/len;
-    ctx.lineTo(x2 - dx*h - dy*w, y2 - dy*h + dx*w);
-    ctx.moveTo(x2, y2);
-    ctx.lineTo(x2 - dx*h + dy*w, y2 - dy*h - dx*w);
+    var len = Math.sqrt((p2.x-p1.x)*(p2.x-p1.x) + (p2.y-p1.y)*(p2.y-p1.y));
+    var dx = (p2.x-p1.x)/len;
+    var dy = (p2.y-p1.y)/len;
+    ctx.lineTo(p2.x - dx*h - dy*w, p2.y - dy*h + dx*w);
+    ctx.moveTo(p2.x, p2.y);
+    ctx.lineTo(p2.x - dx*h + dy*w, p2.y - dy*h - dx*w);
 
     if (doubleArrow) {
-      ctx.moveTo(x1,y1);
-      ctx.lineTo(x1 + dx*h - dy*w, y1 + dy*h + dx*w);
-      ctx.moveTo(x1, y1);
-      ctx.lineTo(x1 + dx*h + dy*w, y1 + dy*h - dx*w);
+      ctx.moveTo(p1.x,p1.y);
+      ctx.lineTo(p1.x + dx*h - dy*w, p1.y + dy*h + dx*w);
+      ctx.moveTo(p1.x, p1.y);
+      ctx.lineTo(p1.x + dx*h + dy*w, p1.y + dy*h - dx*w);
     }
 
     ctx.stroke();
+
+    return new Rect(p1.x, p1.y, p2.x - p1.x, p2.y - p1.y);
   }
 };
